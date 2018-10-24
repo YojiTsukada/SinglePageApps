859a44d9cc7a1298fd0f30e91a3f1d07eab4aa45
YojiTsukada <>
2018-10-24 14:02:33 +0900

    add Dynamo db for increment sequence id.

--- serverless/lambda_function.py
+++ serverless/lambda_function.py
@@ -1,6 +1,42 @@
 import json
+import boto3
+from datetime import datetime
+from boto3.dynamodb.conditions import Key, Attr
 
+# DynamoDB
+dynamodb = boto3.resource('dynamodb')
+
+
+# increment sequence id
+def increment_seq():
+    table = dynamodb.Table('sequence')
+
+    # get sequence id
+    response = table.get_item(
+        Key={
+            'name': 'spa'
+        }
+    )
+
+    id = response['Item']['id']
+    print('Current ID : ' + str(id))
+
+    # Next Sequence id
+    next_id = id + 1
+    
+    # Update sequence id
+    table.put_item(
+    Item={
+        'name': 'spa' ,
+        'id': int(next_id)
+       }
+    )
+
+    return 
+
+    
 def lambda_handler(event, context):
+    increment_seq()
 
     input_message = event['body'].split("&")
     
@@ -9,7 +45,6 @@ def lambda_handler(event, context):
     for n in input_message:
         data = n.split("=")
         request[data[0]] = data[1] 
-
 
     return {
         "statusCode": 200,
